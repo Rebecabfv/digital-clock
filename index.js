2fc20e3795c86d7b535a61ce7aeba870d8b7555f
Rebeca Baptista <re.bfviana@gmail.com>
2022-02-09 11:01:17 -0300

    Adiciona digital clock and date

---
+++ index.js
@@ -0,0 +1,41 @@
+setInterval(showTime, 1000);
+function showTime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  am_pm = "AM";
+
+  if (hour > 12) {
+    hour -= 12;
+    am_pm = "PM";
+  }
+  if (hour == 0) {
+    hr = 12;
+    am_pm = "AM";
+  }
+
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  let currentTime = hour + ":" + min + ":" + sec + am_pm;
+  document.getElementById("clock").innerHTML = currentTime;
+}
+
+setInterval(showDate, 1);
+function showDate() {
+  let date = new Date();
+  let day = date.getDay();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+
+  day = day < 10 ? "0" + day : day;
+  month = month < 10 ? "0" + month : month;
+
+  let currentDate = `${day} / ${month} / ${year}`;
+  document.getElementById("date").innerHTML = currentDate;
+}
+
+showTime();
+showDate();
